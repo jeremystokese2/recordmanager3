4c4fb24bdf639521e52380d24fbc94651134bbe5
Jeremy Stokes <jeremy.stokes@engagesq.com>
2024-10-25 13:29:42 +1100

    persistent sqlite db

--- app/settings.py
+++ app/settings.py
@@ -74,13 +74,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# Database configuration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(os.getenv('RAILWAY_VOLUME_MOUNT_PATH', BASE_DIR), 'db.sqlite3'),
     }
 }
 
